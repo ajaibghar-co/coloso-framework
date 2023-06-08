207ff4edcf52989a61237195705620383fa33921
opheliagame <21999998+opheliagame@users.noreply.github.com>
2023-06-08 21:32:25 +0900

    second day

--- core/buildings/final/block_chain.js
+++ core/buildings/final/block_chain.js
@@ -44,7 +44,13 @@ console.log("sDensity: ", sDensity)
 export function main(coord, context, cursor, buffer, data) {
   let sColors = data.color != -1 ? colors[data.color] : ['white']
   let sPattern1 = data.movement != -1 ? patterns[data.movement] : patterns[0]
-	const t = data.movement != -1 ? context.time * 0.001 : 0
+	let t = data.movement != -1 ? context.time * 0.001 : 0
+  if(data.movement != -1 && data.movement == 0) {
+    t = context.time * 0.01
+  }
+  if(data.movement != -1 && data.movement == 3) {
+    t = context.time * 0.004
+  }
 	const m = Math.min(context.cols, context.rows)
   const a = context.metrics.aspect
 
